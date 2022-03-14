0910b5aa45672d0af0e136af78f0971641937c32
Justin L . Rodriguez <justin.rodriguez121206@gmail.com>
2022-03-14 23:42:08 +0800

    make it better

---
+++ routes/stripe.js
@@ -0,0 +1,14 @@
+const router = require("express").Router()
+const dotenv = require("dotenv").config()
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY_MY)
+
+
+router.post("/payment", (req, res) => {
+    stripe.charges.create({
+        source: req.body.tokenId,
+        amount: req.body.amount,
+        currency: "usd"
+    }, (stripeErr, stripeRes) => stripeErr ? res.status(500).json(stripeErr) : res.status(200).json(stripeRes))
+})
+
+module.exports = router
