c7f6fbd8c99d20b9f61f6506daabd3e0e9c8fbf7
Justin L . Rodriguez <justin.rodriguez121206@gmail.com>
2022-03-15 17:48:25 +0800

    first commit

--- Index.js
+++ Index.js
@@ -26,7 +26,7 @@ app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute)
 
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running");
 });
 
